04b28b0beb5383e7d1e0a45be592fe50c5b389a1
Kris <kristian_dilov@abv.bg>
2022-06-02 20:48:48 +0100

    Changes to IUniswapV2Callee

--- components/swap_traits/IUniswapV2Callee.rs
+++ components/swap_traits/IUniswapV2Callee.rs
@@ -3,7 +3,10 @@
 
 use ink_lang as ink;
 
-#[ink::contract]
-trait IUniswapV2Callee {
-    fn uniswapV2Call( amount: u32, amount1: u32) //Will continue tommorow, find a address and bytes data types
+#[ink::trait_definition]
+pub trait IUniswapV2Callee {
+
+    //All public functions must use this attribute
+    #[ink(message)]
+    fn uniswapV2Call(sender: &AccountId , amount0: &u32, amount1: &u32, data: &[i32]);
 }
