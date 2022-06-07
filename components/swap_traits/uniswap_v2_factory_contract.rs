c0565049e8d8d38397c816a531c6175c2089f441
Kris <kristian_dilov@abv.bg>
2022-06-07 23:53:03 +0100

    Factory Contract - start

---
+++ components/swap_traits/uniswap_v2_factory_contract.rs
@@ -0,0 +1,14 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+use ink_lang as ink;
+
+#[ink::contrct]
+mod UniswapV2Factory{
+    use IUniswapV2Factory;
+    use IUniswapV2Pair;
+
+    use ink_lang as ink;
+
+    
+
+}
