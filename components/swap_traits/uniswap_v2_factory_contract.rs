5fac09573958a8489bb4d164979b77430efdbc53
Kris <kristian_dilov@abv.bg>
2022-06-08 23:53:09 +0100

    Factory Contract - progress

--- components/swap_traits/uniswap_v2_factory_contract.rs
+++ components/swap_traits/uniswap_v2_factory_contract.rs
@@ -3,12 +3,30 @@
 use ink_lang as ink;
 
 #[ink::contrct]
-mod UniswapV2Factory{
-    use IUniswapV2Factory;
-    use IUniswapV2Pair;
+mod uniswap_v2_factory{
+
+    use ink_env::AccountId;
+    use swap_traits::{IUniswapV2Factory};
 
     use ink_lang as ink;
 
+
+    #[ink(storage)]
+    pub struct UniswapV2Factory {
+        fee_to: AccountId,
+        fee_to_setter: AccountId,
+        get_pair: Mapping<AccountId, Mapping<AccountId, AccountId>>,
+        all_pairs: AccountId,
+
+    }
+
+
+    #[ink(event)]
+    pub struct PairCreated {
+        token0: AccountId,
+        token1: AccountId,
+        pair: Balance
+    }
     
 
 }
