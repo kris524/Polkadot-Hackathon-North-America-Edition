e94310176307fba1388fe9f15510d82f5c4178a2
Kris <kristian_dilov@abv.bg>
2022-06-19 21:54:13 +0100

    Math + UQ112x112 libs

--- components/uniswap_v2_erc20/uniswap_v2_pair_contract.rs
+++ components/uniswap_v2_erc20/uniswap_v2_pair_contract.rs
@@ -5,7 +5,8 @@ use ink_lang as ink;
 #[ink::contrct]
 mod uniswap_v2_pair {
     use ink_env::AccountId;
-    use lib::uniswap_v2_erc20;
+    use uniswap_v2_erc20::Math;
+    use uniswap_v2_erc20::uniswap_v2_erc20;
     use swap_traits::{IUniswapV2Pair, IUniswapV2Callee, IUniswapV2Factory, Erc20};
 
 }
