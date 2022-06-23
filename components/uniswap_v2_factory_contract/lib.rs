e54b9398632cc0f3b4cb1f8a5f04b0003b9f84a4
Kris <kristian_dilov@abv.bg>
2022-06-23 23:45:58 +0100

    progress-8

--- components/uniswap_v2_factory_contract/lib.rs
+++ components/uniswap_v2_factory_contract/lib.rs
@@ -4,14 +4,38 @@ use ink_lang as ink;
 
 #[ink::contract]
 mod uniswap_v2_factory{
-
-    use ink_storage::{traits::SpreadAllocate, Mapping};
-
-    // pub use swa
-    pub use swap_traits::{IUniswapV2Factory, UniswapFactoryError, UniswapFactoryResult};
-    // use swap_traits::{IUniswapV2Factory, UniswapFactoryError, UniswapFactoryResult};
-
     use ink_lang as ink;
+    use ink_storage::{traits::SpreadAllocate, Mapping};
+    // pub use swap_traits::{IUniswapV2Factory, UniswapFactoryError, UniswapFactoryResult};
+
+
+    #[ink::trait_definition]
+    pub trait IUniswapV2Factory {
+        #[ink(message)]
+        fn fee_to(&self) -> AccountId;
+
+        #[ink(message)]
+        fn fee_to_setter(&self) -> AccountId;
+
+        #[ink(message)]
+        fn get_pair(&self, toekn_a: AccountId, token_b: AccountId) -> AccountId;
+
+        #[ink(message)]
+        fn all_pairs(&self, log_value: u64) -> AccountId;
+
+        // #[ink(message)]
+        // fn all_pairs_length(&mut self) -> u64;
+
+        // #[ink(message)]
+        // fn create_pair(&mut self, token_a: AccountId, token_b: AccountId) -> AccountId;
+
+        #[ink(message)]
+        fn set_fee_to(&mut self, address: AccountId);
+
+        #[ink(message)]
+        fn set_fee_to_setter(&mut self, address: AccountId);
+    }
+
 
 
 
@@ -20,7 +44,7 @@ mod uniswap_v2_factory{
     pub struct UniswapV2Factory {
         fee_to: AccountId,
         fee_to_setter: AccountId,
-        get_pair: Mapping<AccountId, Mapping<AccountId, AccountId>>,
+        // get_pair: Mapping<AccountId, Mapping<AccountId, AccountId>>,
         all_pairs: AccountId,
         // balance: Balance
 
@@ -40,40 +64,45 @@ mod uniswap_v2_factory{
         pub fn new( _fee_to_setter: AccountId) -> Self{
                 // self.fee_to_setter = self._fee_to_setter;
                 todo!();
-                ink_lang::utils::initialize_contract(|contract| {
-                    Self::new_init(contract, _fee_to_setter)
-                })
+                // ink_lang::utils::initialize_contract(|contract| {
+                //     Self::new_init(contract, _fee_to_setter)
+                // })
         }
-  
+
     }
+  
+    
 
     impl IUniswapV2Factory for UniswapV2Factory {
-        #[ink(message)]
-        fn create_pair(&mut self, token_a: AccountId, token_b: AccountId) -> UniswapFactoryResult<()> {
-                if token_a != token_b {
-                    // Conditional Operator (? :)
-                    // The conditional operator first evaluates an expression for a true or false value and then executes 
-                    // one of the two given statements depending upon the result of the evaluation.
+        
+        // #[ink(message)]                                                 //-> UniswapFactoryResult<()>
+        // fn create_pair(&mut self, token_a: AccountId, token_b: AccountId)  {
+                // if token_a != token_b {
+                //     // Conditional Operator (? :)
+                //     // The conditional operator first evaluates an expression for a true or false value and then executes 
+                //     // one of the two given statements depending upon the result of the evaluation.
 
-                    if token_b > token_a {
-                        let token0: AccountId = token_a;
-                        let token1: AccountId = token_b;
-                    }
-                    else {
-                        let token0: AccountId = token_b;
-                        let token1: AccountId = token_a;
-                    }
+                //     if token_b > token_a {
+                //         let token0: AccountId = token_a;
+                //         let token1: AccountId = token_b;
+                //     }
+                //     else {
+                //         let token0: AccountId = token_b;
+                //         let token1: AccountId = token_a;
+                //     }
                     
                        
                     
-                    todo!();
+                //     todo!();
                     
-                }
-        }
-        #[ink(message)]
-        fn all_pairs_length(&self) -> Balance {
-            return self.all_pairs.length;
-        }
+                // }
+        //         todo!();
+        // }
+        // #[ink(message)]
+        // fn all_pairs_length(&mut self)  {
+        //     // return self.all_pairs.length;
+        //     todo!();
+        // }
         #[ink(message)]
         fn set_fee_to(&mut self, _fee_to: AccountId) {
             let sender = self.env().caller();
@@ -89,7 +118,33 @@ mod uniswap_v2_factory{
             }
         }
 
+        #[ink(message)]
+        fn fee_to(&self) -> AccountId {
+            todo!();
+        }
+
+        #[ink(message)]
+        fn fee_to_setter(&self) -> AccountId {
+            todo!();
+        }
+
+        #[ink(message)]
+        fn get_pair(&self, toekn_a: AccountId, token_b: AccountId) -> AccountId{
+            todo!();
+        }
+
+        #[ink(message)]
+        fn all_pairs(&self, log_value: u64) -> AccountId {
+            todo!();
+        }
+
     }
 
-}
+    #[cfg(test)]
+    mod tests {
+        todo!();
+    }
+
     
+      
+}
